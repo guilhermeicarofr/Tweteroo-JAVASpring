fde8a1a5c80d2a4b934b6a825064feb3a6d899cb
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-04-05 17:04:55 -0300

    fix: fixed route paths and added cross origin permission for controllers

--- src/main/java/com/tweteroo/tweteroo/controller/AuthController.java
+++ src/main/java/com/tweteroo/tweteroo/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.tweteroo.tweteroo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,8 @@ import com.tweteroo.tweteroo.service.AuthService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/sign-up")
+@CrossOrigin
+@RequestMapping("/api/auth/sign-up")
 public class AuthController {
   @Autowired
   private AuthService service;
--- src/main/java/com/tweteroo/tweteroo/controller/TweetController.java
+++ src/main/java/com/tweteroo/tweteroo/controller/TweetController.java
@@ -3,6 +3,7 @@ package com.tweteroo.tweteroo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,8 @@ import com.tweteroo.tweteroo.service.TweetService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/tweets")
+@CrossOrigin
+@RequestMapping("/api/tweets")
 public class TweetController {
   @Autowired
   private TweetService service;
